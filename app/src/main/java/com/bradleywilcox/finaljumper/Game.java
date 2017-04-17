d7b012d016c0880c38f901e31e2101119c6d3e45
Brad <thatbr4d@gmail.com>
2017-04-16 23:59:31 -0400

    add surfaceview and game thread

---
+++ app/src/main/java/com/bradleywilcox/finaljumper/Game.java
@@ -0,0 +1,85 @@
+package com.bradleywilcox.finaljumper;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Brad on 4/16/2017.
+ */
+
+public class Game extends SurfaceView implements Runnable {
+    private Canvas gameCanvas;
+    private Bitmap frameBuffer;
+    private Thread gameThread = null;
+    private SurfaceHolder holder;
+    private volatile boolean running = false;
+
+    public Game(Context context, Bitmap buffer) {
+        super(context);
+        frameBuffer = buffer;
+        gameCanvas = new Canvas(frameBuffer);
+        holder = getHolder();
+    }
+
+    @Override
+    public void run() {
+        Rect rect = new Rect();
+        long startTime = System.nanoTime();
+
+        while (running) {
+            if (!holder.getSurface().isValid())
+                continue;
+
+            float deltaTime = (System.nanoTime() - startTime) / 1000000000.0f;
+
+            startTime = System.nanoTime();
+
+            update(deltaTime);
+            render(deltaTime);
+
+
+            Canvas canvas = holder.lockCanvas();
+            canvas.getClipBounds(rect);
+            canvas.drawBitmap(frameBuffer, null, rect, null);
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void update(float deltaTime) {
+
+    }
+
+    public void render(float deltaTime) {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+
+        //background
+        Rect bg = new Rect(0, 0, 320, 480);
+        gameCanvas.drawRect(bg, paint);
+
+    }
+
+    public void resume() {
+        running = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void pause() {
+        running = false;
+        while (true) {
+            try {
+                gameThread.join();
+                break;
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/bradleywilcox/finaljumper/MainActivity.java
+++ app/src/main/java/com/bradleywilcox/finaljumper/MainActivity.java
@@ -1,13 +1,34 @@
 package com.bradleywilcox.finaljumper;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Bitmap buffer = Bitmap.createBitmap(320, 480, Bitmap.Config.RGB_565);
+
+        game = new Game(this, buffer);
+
+        setContentView(game);
     }
+
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        game.pause();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        game.resume();
+    }
+
 }
