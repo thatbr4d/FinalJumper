4761ffc0c40548a94208fa84913845e6ace7b3ef
MichaelCha <mcha@my.apsu.edu>
2017-04-19 15:12:39 -0500

    draw bitmap

--- app/src/main/java/com/bradleywilcox/finaljumper/Hero.java
+++ app/src/main/java/com/bradleywilcox/finaljumper/Hero.java
@@ -1,20 +1,29 @@
 package com.bradleywilcox.finaljumper;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.drawable.Drawable;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
 
 /**
  * Created by Brad on 4/17/2017.
  */
 
-public class Hero extends GameObject {
+public class Hero extends GameObject{
 
     private static final int JUMP_VELOCITY = -50;
 
     private Paint paint;
     private PointF velocity;
+    private Bitmap character;
+
+
 
     public Hero(int x, int y) {
         this.width = 25;
@@ -23,6 +32,7 @@ public class Hero extends GameObject {
         paint.setColor(Color.RED);
         velocity = new PointF(0, 0);
 
+        //setCharacter();
         this.x = x;
         this.y = y;
     }
@@ -43,5 +53,17 @@ public class Hero extends GameObject {
     public void hitPlatform() {
         velocity.y = JUMP_VELOCITY;
     }
+
+    /*private void setCharacter(){
+        Drawable charDraw = getResources().getDrawable(R.drawable.jump);
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int sizePixels = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width, dm);
+        character = Bitmap.createBitmap(sizePixels, sizePixels, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(character);
+        charDraw.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        charDraw.draw(canvas);
+
+    }*/
 
 }
