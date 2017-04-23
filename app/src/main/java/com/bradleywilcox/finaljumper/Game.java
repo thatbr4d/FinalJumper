774ab8e296fefa2f412db0ada3067c1bbbb44840
MichaelCha <mcha@my.apsu.edu>
2017-04-23 09:54:56 -0500

    popup window displays

--- app/src/main/java/com/bradleywilcox/finaljumper/Game.java
+++ app/src/main/java/com/bradleywilcox/finaljumper/Game.java
@@ -1,17 +1,29 @@
 package com.bradleywilcox.finaljumper;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 /**
  *
@@ -46,6 +58,10 @@ public class Game extends SurfaceView implements Runnable {
     private Paint tempPaint;
 
     private Context context;
+
+    private  ImageView imgPop;
+    private TextView txtPopLose;
+    private Button buttLose1, buttLose2;
 
     public Game(Context context, Bitmap buffer) {
         super(context);
@@ -124,8 +140,43 @@ public class Game extends SurfaceView implements Runnable {
         if(state==true && counter ==1)
         {
             SoundFiles.playSound(3);
+            runPopup2();
         }
     }
+
+    public void runPopup2() {
+
+        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.lose, null);
+        final PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        buttLose1 = (Button) popupView.findViewById(R.id.buttonOver);
+        buttLose2 = (Button) popupView.findViewById(R.id.buttonOver2);
+        txtPopLose = (TextView) popupView.findViewById(R.id.textView3);
+        popupWindow.showAtLocation(popupView, 150, 100, 0);
+
+        buttLose1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+                popupWindow.dismiss();
+            }
+        });
+
+        buttLose2.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent intent = new Intent(context, Start.class);
+                context.startActivity(intent);
+                popupWindow.dismiss();
+            }
+        });
+    }
+
 
     public void resume() {
         running = true;
